478528299f76652a13074a25e8e6f18f28dbf6b7
Victor-Osorgin <63223361+Victor-Osorgin@users.noreply.github.com>
2020-04-22 02:20:49 +0400

    Add files via upload

---
+++ FormLogin/FormLogin/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace FormLogin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Login = new System.Windows.Forms.TextBox();
+            this.Pass = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(95, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Имя Пользователя";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(153, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Пароль";
+            this.label2.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // Login
+            // 
+            this.Login.HideSelection = false;
+            this.Login.Location = new System.Drawing.Point(216, 123);
+            this.Login.Name = "Login";
+            this.Login.Size = new System.Drawing.Size(199, 20);
+            this.Login.TabIndex = 2;
+            this.Login.TextChanged += new System.EventHandler(this.Login_TextChanged);
+            // 
+            // Pass
+            // 
+            this.Pass.Location = new System.Drawing.Point(216, 165);
+            this.Pass.Name = "Pass";
+            this.Pass.Size = new System.Drawing.Size(199, 20);
+            this.Pass.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(368, 203);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(47, 27);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Выход";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(216, 203);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(47, 27);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Вход";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(216, 252);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(61, 252);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(139, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Временно заболокирован";
+            this.label4.Visible = false;
+            this.label4.Click += new System.EventHandler(this.Label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 457);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Pass);
+            this.Controls.Add(this.Login);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Login;
+        private System.Windows.Forms.TextBox Pass;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ FormLogin/FormLogin/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormLogin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+       
+        int x = 0;
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (Login.Text == "user" && Pass.Text == "010100")
+            {
+                Form2 s = new Form2();
+                s.Show();
+
+                this.Hide();
+            }
+            else
+            {
+                Login.Text = "";
+                Pass.Text = "";
+                x++;
+                MessageBox.Show("Неверное Имя Пользователя или Пароль");
+            }
+
+            if (x == 3)
+            {
+                timer1.Enabled = true;
+                Login.ReadOnly = !Login.ReadOnly;
+                Pass.ReadOnly = !Pass.ReadOnly;
+                label4.Visible = true;
+                label3.Visible = true;
+            }
+            else
+            {
+               int  x = 0;
+                
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        int i = 60;
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (i == 0)
+            {
+                Login.ReadOnly = !Login.ReadOnly;
+                Pass.ReadOnly = !Pass.ReadOnly;
+                timer1.Enabled = false;
+                label4.Visible = false;
+                label3.Visible = false;
+
+                i = 60;
+                x = 0;
+            }
+            else
+            {
+                i--;
+                
+            }
+            label3.Text = i.ToString();
+            
+        }
+
+        private void Login_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
